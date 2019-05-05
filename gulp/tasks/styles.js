d1f65249eb3b1bd0736b334ac492683ecea600bc
C <chanakkarki3@gmail.com>
2019-05-05 16:12:48 +0100

    mobile friendly using postcss-mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,10 +4,16 @@ postcss = require ('gulp-postcss'),
 autoprefixer = require ('autoprefixer'),
 cssvar = require('postcss-simple-vars'),
 nested = require ('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('css',function(){
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssImport, cssvar,nested, autoprefixer]))	
-    .pipe(gulp.dest('./app/tenp/styles'));
+	.pipe(postcss([cssImport,mixins, cssvar,nested, autoprefixer]))	
+	.on('error', function(errorInfo){
+		console.log(errorInfo.toString());
+		this.emit('end');
+	})
+
+	.pipe(gulp.dest('./app/tenp/styles'));
 });
